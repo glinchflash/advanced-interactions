edc32a38f37c665310799159c246f55c2bc7b726
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-31 11:17:15 +0200

    fixing bug with images on github Pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //variable list for parallax carousel
-let imagesCarousel = ['../images/carousel/umbreon.jpg', '../images/carousel/mewtwo.jpg', '../images/carousel/scizor.jpg', '../images/carousel/starters.jpg'];
+let imagesCarousel = ['./images/carousel/umbreon.jpg', './images/carousel/mewtwo.jpg', './images/carousel/scizor.jpg', './images/carousel/starters.jpg'];
 let carousel = document.getElementById('carouselContainer');
 let activeImg = 0;
 let next = document.querySelector('.next');
@@ -11,28 +11,27 @@ carousel.style.backgroundRepeat = 'no-repeat'
 carousel.style.backgroundSize = 'cover';
 carousel.style.backgroundAttachment = 'fixed';
 
-next.addEventListener('click',()=>{
+next.addEventListener('click', () => {
     if (activeImg < imagesCarousel.length - 1) {
         activeImg++
         carousel.style.backgroundImage = `url(${imagesCarousel[activeImg]})`
-    } else if(activeImg === imagesCarousel.length-1){
+    } else if (activeImg === imagesCarousel.length - 1) {
         activeImg = 0;
         carousel.style.backgroundImage = `url(${imagesCarousel[activeImg]})`
     }
 
 });
 
-prev.addEventListener('click', ()=>{
-    if (activeImg > 0){
+prev.addEventListener('click', () => {
+    if (activeImg > 0) {
         activeImg--;
         carousel.style.backgroundImage = `url(${imagesCarousel[activeImg]})`;
     } else {
-        activeImg = imagesCarousel.length-1;
+        activeImg = imagesCarousel.length - 1;
         carousel.style.backgroundImage = `url(${imagesCarousel[activeImg]})`;
     }
 
 });
-
 
 
 //collage
@@ -40,7 +39,7 @@ const collage = document.getElementsByClassName("image");
 for (let i = 0; i < collage.length; i++) {
     const img = document.createElement("img");
     let click = false;
-    img.src = `../images/collage/${i + 1}.png`;
+    img.src = `./images/collage/${i + 1}.png`;
     img.addEventListener('click', function () {
         this.style.transform = "scale(1.2)";
         document.getElementById(`${i + 1}`).style.display = "block";
@@ -59,13 +58,12 @@ for (let i = 0; i < collage.length; i++) {
 
 //hover popups
 const hoverMons = document.querySelectorAll('.poke');
-console.log(hoverMons);
-hoverMons.forEach((name) =>{
-    name.addEventListener('mouseover',()=>{
+hoverMons.forEach((name) => {
+    name.addEventListener('mouseover', () => {
         let sprite = new Image();
-        sprite.src = `../images/popups/${name.innerText}.png`;
+        sprite.src = `./images/popups/${name.innerText}.png`;
         sprite.style.position = 'absolute';
-        sprite.style.left ='0%';
+        sprite.style.left = '0%';
         sprite.style.transform = 'translateY(-100%)';
         sprite.style.height = '300%'
         name.appendChild(sprite)
@@ -84,7 +82,7 @@ const onMouseMove = (e) => {
     if (e.pageX < 1370 && e.pageX > 100) {
         chaser.style.left = e.pageX + 'px';
     }
-   if (e.pageY < 2620 && e.pageY > 2325) {
+    if (e.pageY < 2620 && e.pageY > 2325) {
         chaser.style.top = e.pageY + 'px';
     }
 
@@ -92,7 +90,6 @@ const onMouseMove = (e) => {
 
 
 document.addEventListener('mousemove', onMouseMove);
-
 
 //random letters
 Array.from(document.querySelectorAll(".letter")).forEach(el => {
